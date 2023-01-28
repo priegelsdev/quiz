c91e498db01679a81beca262fbd458bda4e91c46
priegelsdev <priegelsdev@gmail.com>
2023-01-28 11:22:05 +0100

    write some logic to decode json res and try to push answers to custom array to be shuffled

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import blobTwo from './assets/img/blob2.png'
 
 export default function App() {
   const [gameStart, setGameStart] = useState(false)
+  const [questions, setQuestions] = useState([])
 
   // function to handle button click as button stays on page to deal with different things
   function handleClick() {
@@ -16,6 +17,94 @@ export default function App() {
       console.log('restart game')
     }
   }
+
+  // function to decode JSON output from API to turn encoded characters into readable chars
+  function htmlDecode(input) {
+    let doc = new DOMParser().parseFromString(input, "text/html");
+    return doc.documentElement.textContent
+  }
+
+  function decodeRes(array) {
+    array.forEach()
+  }
+
+  // helper function to shuffle array of answers
+  function shuffle(array) {
+    for (let i = array.length -1; i > 0; i--) {
+      let j = Math.floor(Math.random() * i)
+      let k = array[i]
+      array[i] = array[j]
+      array[j] = k
+    }
+    return array
+  }
+
+/* 
+  {
+    question: 'q',
+    answers: [{
+      answer: answer,
+      isCorrect: true,
+      isLogged: false,
+      isShown: false
+    },
+    {
+      answer: incorrect_answer,
+      isLogged: false,
+      isShown: false
+    }]
+  }
+*/
+
+
+  // effect to fetch questions on game start
+  useEffect(() => {
+    fetch('https://opentdb.com/api.php?amount=5&type=multiple', 
+      {
+        method: "GET",
+/*         headers: {
+          'Accept': 'application/json, charset=UTF-8',
+          "Content-Type": "application/json; charset=UTF-8;",
+          'Accept-Charset': 'utf-8'
+        } */
+      })
+        .then(res => res.json())
+        .then(data => {
+
+          data.results.map(result => {
+            const allAnswers = []
+            const incorrectAnswers = []
+            allAnswers.push(htmlDecode(result.correct_answer))
+            result.incorrect_answers.map(wrongAnswer => incorrectAnswers.push(htmlDecode(wrongAnswer)))
+            
+            console.log(incorrectAnswers)
+            console.log(allAnswers)
+          })
+
+
+          const questions = data.results.map(result => {
+            const answersArray = shuffle(result) 
+          })
+
+
+/*           const questions = data.results.map(result => (
+            { 
+              question: htmlDecode(result.question),
+              answers: [{
+
+              }],
+
+              correctAnswer: htmlDecode(result.correct_answer),
+              incorrectAnswers: htmlDecode(result.incorrect_answers),
+            }
+            )) */
+
+          setQuestions(questions)
+          console.log(data.results)
+
+
+        })
+  }, [gameStart])
 
   return (
     <main>
