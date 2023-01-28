673726058276b2b141fd1366c9581933d4411be2
priegelsdev <priegelsdev@gmail.com>
2023-01-28 10:01:35 +0100

    set up intro screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react'
+import Intro from './Components/Intro'
+import blobOne from './assets/img/blob1.png'
+import blobTwo from './assets/img/blob2.png'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <main>
+      <img className="blob-one" src={blobOne}/>
+      <Intro />
+      <button 
+        className="check-btn" 
+/*         onClick={handleClick} */
+      >
+        Start Quiz
+      </button> 
+      <img className="blob-two" src={blobTwo}/>
+    </main>
   )
 }
-
-export default App
---
+++ src/Components/Intro.jsx
@@ -0,0 +1,8 @@
+export default function Intro() {
+  return (
+    <div className="intro-details">
+      <h1 className="intro-title">Quizzical</h1>
+      <p className="intro-description">This is a placeholder text for a possible description to be added</p>
+    </div>
+  )
+}
