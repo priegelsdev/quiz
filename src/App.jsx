1b73c9174b6f8026575db393ba33d24881e807e7
priegelsdev <priegelsdev@gmail.com>
2023-01-28 10:08:40 +0100

    add some logic to button

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,28 @@ import blobOne from './assets/img/blob1.png'
 import blobTwo from './assets/img/blob2.png'
 
 export default function App() {
+  const [gameStart, setGameStart] = useState(false)
+
+  // function to handle button click as button stays on page to deal with different things
+  function handleClick() {
+    if (!gameStart) {
+      setGameStart(true)
+    } else if (gameStart) {
+      console.log('check answers')
+    } else {
+      console.log('restart game')
+    }
+  }
+
   return (
     <main>
       <img className="blob-one" src={blobOne}/>
-      <Intro />
+      {!gameStart && <Intro />}
       <button 
         className="check-btn" 
-/*         onClick={handleClick} */
+        onClick={handleClick}
       >
-        Start Quiz
+        Start Quiz {/* add logic so text will show check answers or restart game later on*/}
       </button> 
       <img className="blob-two" src={blobTwo}/>
     </main>
